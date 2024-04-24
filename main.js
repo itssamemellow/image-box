32a72c2509c63fde9b65280d664799c8ab6cb4e2
boitumelo62 <boitumelomaphalala62@gmail.com>
2024-04-24 07:40:59 +0200

    class update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let hero = document.querySelector("main section");
+/* let hero = document.querySelector("main section");
 let photo = document.querySelector("main section img");
 
 hero.addEventListener("click", function () {
@@ -12,4 +12,31 @@ hero.addEventListener("click", function () {
   setTimeout(function () {
     photo.classList.remove("hide");
   }, 2000);
+});*/
+
+const hero = document.querySelector("main section");
+const foto = document.getElementById("foto");
+
+let areWeLoadingAPhotograph = false;
+
+hero.addEventListener("click", () => {
+  if (areWeLoadingAPhotograph == false) {
+    areWeLoadingAPhotograph = true;
+    foto.classList.add("hide");
+
+    const random = Math.round(Math.random() * 1000);
+
+    console.log(random);
+
+    const fotoURL = `https://picsum.photos/seed/${random}/1000/600`;
+
+    foto.onload = () => {
+      areWeLoadingAPhotograph = false;
+      foto.classList.remove("hide");
+    };
+
+    foto.src = fotoURL;
+  } else {
+    alert("Photographed is being loaded, please wait.");
+  }
 });
